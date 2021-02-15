5706efb58c675f7760335a8e9725d7aa10a86904
Wiktoria <wiktoriajuliasalamon@gmail.com>
2021-02-15 21:45:52 +0100

    Clean repository

--- studybuddy-front/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-    baseURL: process.env.REACT_APP_PROXY_API || 'http://localhost:8000/',
-})
-
-export const endpoints = {
-    users: 'users/'
-}
