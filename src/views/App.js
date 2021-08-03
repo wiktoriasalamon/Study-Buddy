127e263a7c0d05d2442c9bce9edbcacb539c0e63
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2021-08-03 19:55:11 +0200

    Test husky

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,13 @@
 import logo from 'assets/logo.svg';
 import './App.css';
 
-console.log('hello');
+console.log('hello')
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
